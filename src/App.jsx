619a7a762f4449b61816f1f4d6657c66d0c539b4
Malay Mahata <malaymahata864@gmail.com>
2026-02-06 23:00:44 +0530

    Initial commit: Todo list project

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import { useState, useCallback } from 'react'
+import './App.css'
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+ 
+  const addTask = useCallback(() => {
+    
+    if (newTask.trim() !== "") {
+      setTasks(prevTasks => [...prevTasks, newTask]);
+      setNewTask("");
+    }
+  }, [newTask]);
+
+  
+  const deleteTask = useCallback((index) => {
+    
+    setTasks(prevTasks => prevTasks.filter((_, i) => i !== index));
+  }, []);
+
+
+  const moveTaskUp = useCallback((index) => {
+   
+    if (index > 0) {
+      setTasks(prevTasks => {
+        const updatedTasks = [...prevTasks];
+        [updatedTasks[index], updatedTasks[index - 1]] = 
+        [updatedTasks[index - 1], updatedTasks[index]];
+        return updatedTasks;
+      });
+    }
+  }, []);
+
+ 
+  const moveTaskDown = useCallback((index) => {
+    
+    setTasks(prevTasks => {
+      if (index < prevTasks.length - 1) {
+        const updatedTasks = [...prevTasks];
+        [updatedTasks[index], updatedTasks[index + 1]] = 
+        [updatedTasks[index + 1], updatedTasks[index]];
+        return updatedTasks;
+      }
+      return prevTasks;
+    });
+  }, []);
+
+  return (
+    <div className="to-do-list">
+      <h1>To-Do List</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Enter a task..."
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+            onKeyDown={(e) => e.key === 'Enter' && addTask()}
+          
+        />
+        <button className="add-button" onClick={addTask}>Add</button>
+      </div>
+
+      <ol>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <span className="text">{task}</span>
+            <div className="button-group">
+              <button className="delete-button" onClick={() => deleteTask(index)}>Delete</button>
+              <button className="move-button" onClick={() => moveTaskUp(index)}>UP</button>
+              <button className="move-button" onClick={() => moveTaskDown(index)}>DOWN</button>
+            </div>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+}
+
+export default App;
